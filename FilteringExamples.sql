e25227d0f9d290a06c8d655c999f9e2591bd5d45
Alexa Summers <alexas0202@gmail.com>
2021-03-04 12:02:57 -0600

    Complete Filtering Demos
    Final commit of filtering demos

--- FilteringExamples.sql
+++ FilteringExamples.sql
@@ -48,6 +48,91 @@ FROM Employees
 WHERE EmpStart_Date BETWEEN '2002-01-01' AND '2003-12-31';
 
 -- --------------------------------------------------------------------------------------------------------------------------
--- Slide 11 (Membership Condition)
+-- Slide 11 & 12 (Membership Condition)
 -- --------------------------------------------------------------------------------------------------------------------------
 
+SELECT Account_ID, Account_Type_ID,Customer_ID
+FROM Accounts
+WHERE Account_Type_ID='CHK' OR Account_Type_ID='SAV' OR Account_Type_ID='CD';
+
+SELECT Account_ID, Account_Type_ID,Customer_ID -- IN operator
+FROM Accounts
+WHERE Account_Type_ID IN ('CHK', 'SAV', 'CD');
+
+SELECT Account_ID, Account_Type_ID,Customer_ID -- NOT IN operator
+FROM Accounts
+WHERE Account_Type_ID NOT IN ('CHK', 'SAV', 'CD');
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 13 & 14 (Problem & Solution)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+SELECT Account_ID, Account_Type_ID,Employee_ID
+FROM Accounts
+WHERE Employee_ID IN (SELECT Employee_ID FROM Employees WHERE YEAR(EmpStart_Date)=2002);
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 15 (Any Operator)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+SELECT * 
+FROM Transactions
+WHERE TrnsAmount > ANY -- ANY operator returns true if any of the subquery meets the conditions
+(
+	SELECT MAX(TrnsAmount)
+	FROM Transactions
+	GROUP BY TrnsType
+);
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 16 (ALL Operator)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+SELECT * 
+FROM Transactions
+WHERE TrnsAmount >= ALL -- ALL operator returns true if all of the subquery meets the conditions
+(
+SELECT MAX(TrnsAmount)
+FROM Transactions
+GROUP BY TrnsType
+);
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 18 & 19 (Wildcards)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+SELECT EmpLast_Name 
+FROM Employees
+WHERE EmpLast_Name LIKE '_a%e%';
+
+SELECT EmpLast_Name 
+FROM Employees
+WHERE EmpLast_Name LIKE '_a%e%' COLLATE utf8mb4_bin;
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 20 & 21 (Problem & Solution)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+SELECT EmpFirst_Name,EmpLast_Name
+FROM Employees
+WHERE EmpLast_Name LIKE 'F%' COLLATE utf8mb4_bin  OR EmpLast_Name LIKE 'G%' COLLATE utf8mb4_bin;
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 18 & 19 (RegEx)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+SELECT Employee_ID, EmpFirst_Name, EmpLast_Name 
+FROM Employees
+WHERE EmpLast_Name REGEXP '^[FG]';
+
+SELECT Employee_ID, EmpFirst_Name, EmpLast_Name 
+FROM Employees
+WHERE EmpLast_Name REGEXP BINARY '^[FG]';
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 24 & 25 (Problem & Solution)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+SELECT Employee_ID, EmpFirst_Name, EmpLast_Name 
+FROM Employees
+WHERE EmpLast_Name REGEXP '^[^m]+$';
