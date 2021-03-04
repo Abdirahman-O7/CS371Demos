59acd6184a4f8a9f706480bb57bb4b2600d060a0
Alexa Summers <alexas0202@gmail.com>
2021-03-04 15:01:39 -0600

    Final commit for lecture 17
    Containing update and delete information

--- UpdatingAndDeletingDataExamples.sql
+++ UpdatingAndDeletingDataExamples.sql
@@ -1,3 +1,68 @@
 -- --------------------------------------------------------------------------------------------------------------------------
 -- Updating & Deleting Data (Lecture 17)
 -- --------------------------------------------------------------------------------------------------------------------------
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 2 (Updating Data)
+-- --------------------------------------------------------------------------------------------------------------------------
+SELECT *
+FROM Employees;
+
+UPDATE Employees 
+SET EmpFirst_Name='Scott', EmpLast_Name='Johns'
+WHERE Employee_ID='1';
+
+SELECT *
+FROM Employees;
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slides 3-5 (Problem & Solution)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+UPDATE Accounts 
+INNER JOIN Branches
+ON Accounts.Branch_ID=Branches.Branch_ID
+SET AcctStatus='CLOSED'
+WHERE Branches.State_ID='NH';
+
+SELECT *
+FROM Accounts
+INNER JOIN Branches
+ON Accounts.Branch_ID=Branches.Branch_ID;
+
+
+UPDATE Accounts -- Same result as line 22
+SET AcctStatus='CLOSED'
+WHERE Accounts.Branch_ID IN (
+ SELECT Branch_ID FROM Branches
+ WHERE State_ID='NH'
+);
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slide 6 (Deleting Data)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+SELECT *
+FROM Customers;
+
+DELETE FROM Customers 
+WHERE Customer_ID='11';
+
+SELECT *
+FROM Customers;
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- Slides 7-9 (Problem & Solution)
+-- --------------------------------------------------------------------------------------------------------------------------
+
+DELETE A FROM Accounts A
+INNER JOIN Branches B
+ON A.Branch_ID=B.Branch_ID
+WHERE B.State_ID='NH';
+
+DELETE FROM Accounts -- Same result as above
+WHERE Accounts.Branch_ID IN (
+ SELECT Branch_ID FROM Branches
+ WHERE State_ID='NH'
+);
+
